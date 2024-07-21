723c2c9d42aaa98fe1593f3b13baab8de469b173
abhay0933 <abhay80912@gmail.com>
2024-07-21 18:14:32 +0530

    Switch to bcryptjs and ensure correct start script

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -1,6 +1,6 @@
 const UserSchema = require('../models/userModel');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 
